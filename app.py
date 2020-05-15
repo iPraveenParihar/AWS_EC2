ce5732978607d63d4d27746f6b19a1a0aff0ac1f
praveen <praveenparihar687@gmail.com>
2020-05-15 18:04:51 +0530

    initial

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+from flask import Flask, request
+
+import model.predict
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "Index Page"
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+	data = request.form.get('data')
+	if data == None:
+		return 'Got None'
+	else:
+		prediction = model.predict.predict([data])
+
+	return json.dumps(str(prediction))
+
+if __name__ == "__main__":
+	app.run()
