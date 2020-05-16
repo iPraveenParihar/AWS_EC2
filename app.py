910548129da0af232bc53d39d339b43fa8f528ed
praveen <praveenparihar687@gmail.com>
2020-05-16 10:14:14 +0530

    fixes

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import json
 import requests
+import tensorflow as tf
+import pickle
 
 from flask import Flask, request
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-import model.predict
+model = None
+tokenizer = None
 
 app = Flask(__name__)
 
@@ -17,10 +24,17 @@ def get_predict():
 	if data == None:
 		return 'Got None'
 	else:
-		prediction = model.predict.predict([data])
+		prediction = model.predict([data])
 
 	return json.dumps(str(prediction))
 
+def load_model():
+	model = tf.keras.models.load_model('model/sms_spam_model.h5')
+
+	with open('model/tokenizer.pickle','rb') as handle:
+		tokenizer = pickle.load(handle)
+
 
 if __name__ == "__main__":
+	load_model()
 	app.run(host='127.0.0.1', debug=True)
